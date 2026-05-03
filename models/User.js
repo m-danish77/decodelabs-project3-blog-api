182d5310d364bf9648a626efae62c71bbcc6b5a9
m-danish77 <mmunibdanish456@gmail.com>
2026-05-03 11:00:00 +0500

    2 - Make User Model and add pre hook to hashed password before saving in database

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
